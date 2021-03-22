9fa643ae7ecd626b47cc5735ca830f78087dfd6d
carlpaton <carl.paton@gmail.com>
2021-03-23 11:15:24 +1300

    Site updated: 2021-03-23 11:15:21

--- js/search.js
+++ js/search.js
@@ -34,27 +34,25 @@ var searchFunc = function (path, search_id, content_id) {
         }
         // perform local searching
         datas.forEach(function (data) {
-          var isMatch = false;
+          var isMatch = true;
+          var content_index = [];
           var data_title = data.title.trim().toLowerCase();
           var data_content = data.content
             .trim()
             .replace(/<[^>]+>/g, "")
             .toLowerCase();
-
           var data_url = data.url;
           var index_title = -1;
           var index_content = -1;
           var first_occur = -1;
           // only match artiles with not empty titles and contents
-          if ((data_title !== "") && (data_content !== "")) {
-
+          if (data_title != "" && data_content != "") {
             keywords.forEach(function (keyword, i) {
               index_title = data_title.indexOf(keyword);
               index_content = data_content.indexOf(keyword);
               if (index_title < 0 && index_content < 0) {
                 isMatch = false;
               } else {
-                isMatch = true;
                 if (index_content < 0) {
                   index_content = 0;
                 }
@@ -75,8 +73,8 @@ var searchFunc = function (path, search_id, content_id) {
             var content = data.content.trim().replace(/<[^>]+>/g, "");
             if (first_occur >= 0) {
               // cut out 100 characters
-              var start = 1;
-              var end = 40;
+              var start = first_occur - 17;
+              var end = first_occur + 80;
               if (start < 0) {
                 start = 0;
               }
